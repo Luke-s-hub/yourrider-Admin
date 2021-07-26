072e23f8abfff256defc7495e847ef28655fb809
azizganiyu <azizganiyu0@gmail.com>
2021-07-26 12:18:53 +0100

    update

--- src/app/pages/vendors/vendors.component.ts
+++ src/app/pages/vendors/vendors.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HelperService } from 'src/app/services/helper/helper.service';
 
@@ -10,26 +11,50 @@ import { HelperService } from 'src/app/services/helper/helper.service';
 })
 export class VendorsComponent implements OnInit {
 
-  vendorData: any
+  showModal: boolean = false
+  details: any
+  riderData: any
+  views: any = []
+  timeout: any
+  companies: any = []
+  filterForm = this.fb.group({
+    quick: [''],
+    date: [''],
+    company: [null],
+  })
 
   constructor(
-    private helper: HelperService,
+    private fb: FormBuilder,
+    public helper: HelperService,
     private http: HttpClient,
     private router: Router
   ) { 
-    this.getVendorStats()
+    this.getRiderStats()
+    this.getCompany()
+    this.filterForm.valueChanges.subscribe((value) => {
+      this.filterData()
+    })
   }
 
   ngOnInit(): void {
   }
 
-  getVendorStats(){
+  getCompany(){
+    this.http.get(
+      this.helper.getApiUrl()+'dashboard/get_companies',
+      {headers: this.helper.header()}
+    ).subscribe((data: any) => {
+      this.companies = data.data
+    })
+  }
+
+  getRiderStats(){
     this.http.get(
       this.helper.getApiUrl()+'dashboard/rider',
       {headers: this.helper.header()}
     ).subscribe((data: any) => {
-      this.vendorData = data.data
-      console.log('data gotten', data.data)
+      this.riderData = data.data
+      this.views = data.data.all
     })
   }
 
@@ -39,7 +64,7 @@ export class VendorsComponent implements OnInit {
         this.helper.getApiUrl()+'dashboard/user/enable/'+id,
         {headers: this.helper.header()}
       ).subscribe((data: any) => {
-        this.getVendorStats()
+        this.getRiderStats()
         this.helper.showSuccess('', data.message)
       })
     }
@@ -52,11 +77,64 @@ export class VendorsComponent implements OnInit {
         this.helper.getApiUrl()+'dashboard/user/disable/'+id,
         {headers: this.helper.header()}
       ).subscribe((data: any) => {
-        this.getVendorStats()
+        this.getRiderStats()
         this.helper.showSuccess('', data.message)
       })
     }
 
+  }
+
+  filterData(){
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      let result = this.riderData.all
+
+      if(this.filterForm.value.quick){
+        result = result.filter((value) => {
+          if(
+            (value.user.name).toLowerCase().includes((this.filterForm.value.quick).toLowerCase())
+            ||
+            (value.user.email).toLowerCase().includes((this.filterForm.value.quick).toLowerCase())
+            ){
+            return value
+          }
+        })
+      }
+      else{
+        console.log('no quick')
+      }
+
+      if(this.filterForm.value.date){
+        result = result.filter((value) => {
+          if(new Date(value.user.created_at) == new Date (this.filterForm.value.date)){
+            return value
+          }
+        })
+      }
+      else{
+        console.log('no date')
+      }
+
+      if(this.filterForm.value.company){
+        result = result.filter((value) => {
+          if(value.company_id == this.filterForm.value.company){
+            return value
+          }
+        })
+      }
+      
+      else{
+        console.log('no company')
+      }
+
+      this.views = result
+    }, 1000)
+  }
+
+  viewInfo(data){
+    this.showModal = true
+    this.details = data
+    console.log(data)
   }
 
 }
